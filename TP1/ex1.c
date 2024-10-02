ce6d5c21635fbb3dbe73d75c17e8d28cef4c359a
Raphael Berquier <raphael.berquier@gmail.com>
2024-10-02 14:21:27 +0200

    Ajout ex 1

---
+++ TP1/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+0 : 0
+1 : 1
+2 : 1+3 = 4
+3 : 1+3+5 = 9
+4 : 1+3+5+9 = 16
+*/
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += (i*2) + 1;
+    }
+    
+    printf("La somme des %d premiers nombres impairs est : %d \n", n, sum);
+    return 0;
+}
--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+
+int t = 3;
+
 // Main function: entry point for execution
 int main() {
 
     // writing print statement to print hello world
-    printf("Hello World !\n");
-
+    printf("Hello World ! -> %d\n", t);
+    
     return 0;
 }
