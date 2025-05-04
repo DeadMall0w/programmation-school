4d95e902fcc3edc0e76436dbc8a9b4d1a865e74f
Raphael Berquier <raphael.berquier@gmail.com>
2025-05-04 22:01:55 +0200

    Debut ctp 2022 backlog

---
+++ Web/Backend/ctp-backend-2022-backlog/templates/accueil.php
@@ -0,0 +1,41 @@
+<?php
+
+// Si la page est appelée directement par son adresse, on redirige en passant par la page index
+if (basename($_SERVER["PHP_SELF"]) != "index.php")
+{
+  header("Location:../index.php");
+  die("");
+}
+
+?>
+
+<h2>Contenu du backlog</h2>
+
+<?php
+
+/* //TODO :
+- Recup la liste des tâches
+- Les afficher
+- Changer la couleur selon l'avancement
+
+*/
+
+
+$taches = ListeTaches(); // fonction présente dans modele et qui fait une requête à la base de donnée
+
+foreach ($taches as $index => $tache) {
+  if ($tache["statut"] == 0){
+    mkDiv($tache["nom"] . " - non commencé", "white");
+  }else if ($tache["statut"] == 1){
+    mkDiv($tache["nom"] . " - commencé", "orange");
+  }else if ($tache["statut"] == 2){
+    mkDiv($tache["nom"] . " - terminé", "green");
+  }else if ($tache["statut"] == 3){
+    mkDiv($tache["nom"] . " - abandonné", "black");
+  }
+}
+// $taches = ["nom" => "Alice", "age" => 25];
+
+// tprint($taches);
+?>
+
